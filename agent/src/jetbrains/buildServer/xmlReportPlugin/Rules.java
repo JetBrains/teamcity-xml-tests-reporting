a244c57c30f43606eba842409c5c7a4c8acb3eeb
Victory.Bedrosova <victory.bedrosova@jetbrains.com>
2011-04-20 19:28:47 +0000

    simplified

--- agent/src/jetbrains/buildServer/xmlReportPlugin/Rules.java
+++ agent/src/jetbrains/buildServer/xmlReportPlugin/Rules.java
@@ -32,103 +32,98 @@ import org.jetbrains.annotations.Nullable;
  * Date: 16.12.10
  * Time: 13:19
  */
-public class Rules extends FileRuleSet<FileRule, FileRule> {
-  private Set<File> myRootIncludePaths;
-
-  private static List<String> resolveRules(@NotNull File baseDir, @NotNull Collection<String> body) {
-    final List<String> resolved = new ArrayList<String>();
-    for (String rule : body) {
-      resolved.add(getRulePrefix(rule) + FileUtil.resolvePath(baseDir, getRulePath(rule)).getPath());
-    }
-    return resolved;
-  }
-
-  private static String getRulePrefix(@NotNull String rule) {
-    if (rule.startsWith("+:")) return "+:";
-    if (rule.startsWith("-:")) return "-:";
-    return "";
-  }
-
-  private static String getRulePath(@NotNull String rule) {
-    if (rule.startsWith("+:") || rule.startsWith("-:")) return rule.substring(2);
-    return rule;
-  }
+public class Rules {
+  @NotNull
+  private final FileRuleSet<FileRule, FileRule> myRules;
+  private Collection<File> myRootPaths;
 
   public Rules(@NotNull Collection<String> body) {
-    super(new ArrayList<String>(body));
+    this(null, body);
   }
 
-  public Rules(@NotNull File baseDir, @NotNull Collection<String> body) {
-    super(resolveRules(baseDir, body));
-  }
+  public Rules(@Nullable final File baseDir, @NotNull Collection<String> body) {
+    myRules = new FileRuleSet<FileRule, FileRule>(new ArrayList<String>(body)) {
 
-  @Override
-  protected void doPostInitProcess(@NotNull List<FileRule> includeRules, @NotNull List<FileRule> excludeRules) {
-    sortByFrom(includeRules, true);
-  }
-
-  @Override
-  protected FileRule createNewIncludeRule(@NotNull String line) {
-    return createRule(line, true);
-  }
-
-  private FileRule createRule(@NotNull String line, boolean isInclude) {
-    return new FileRule<Rules>(line, null, this, isInclude);
-  }
-
-  @Override
-  protected FileRule createExcludeRule(@NotNull String line) {
-    return createRule(line, false);
-  }
-
-  @Override
-  protected FileRule createNewIncludeRule(@NotNull FileRule includeRule) {
-    return createNewIncludeRule(includeRule.getFrom());
-  }
-
-  @Override
-  protected FileRule createNewExcludeRule(@NotNull FileRule excludeRule) {
-    return createExcludeRule(excludeRule.getFrom());
-  }
-
-  private Set<File> initRootIncludePaths() {
-    final ArrayList<FileRule> resultRules = new ArrayList<FileRule>();
-    resultRules.addAll(getIncludeRules());
-
-    final Set<FileRule> processedRules = new HashSet<FileRule>();
-
-    for (Iterator<FileRule> iterator = resultRules.iterator(); iterator.hasNext();) {
-      FileRule rule = iterator.next();
-
-      if (!shouldInclude(rule.getFrom())) {
-        iterator.remove();
-        continue;
+      @Override
+      protected void doPostInitProcess(final List<FileRule> includeRules, final List<FileRule> excludeRules) {
+        sortByFrom(includeRules, true);
+        initRootIncludePaths();
       }
 
-      for (FileRule processed : processedRules) {
-        if (isSubDir(rule.getFrom(), processed.getFrom())) {
-          iterator.remove();
-          break;
+      @Override
+      protected FileRule createNewIncludeRule(final String line) {
+        return createRule(line, true);
+      }
+
+      @Override
+      protected FileRule createExcludeRule(final String line) {
+        return createRule(line, false);
+      }
+
+      @Override
+      protected FileRule createNewIncludeRule(final FileRule includeRule) {
+        return createNewIncludeRule(includeRule.getFrom());
+      }
+
+      @Override
+      protected FileRule createNewExcludeRule(final FileRule excludeRule) {
+        return createExcludeRule(excludeRule.getFrom());
+      }
+
+      private FileRule createRule(@NotNull String line, boolean isInclude) {
+        return new FileRule(resolvePath(line, baseDir) , null, this, isInclude);
+      }
+
+      private void initRootIncludePaths() {
+        final ArrayList<FileRule> resultRules = new ArrayList<FileRule>();
+        resultRules.addAll(getIncludeRules());
+
+        final Set<FileRule> processedRules = new HashSet<FileRule>();
+
+        for (Iterator<FileRule> iterator = resultRules.iterator(); iterator.hasNext();) {
+          FileRule rule = iterator.next();
+
+          if (!shouldInclude(rule.getFrom())) {
+            iterator.remove();
+            continue;
+          }
+
+          for (FileRule processed : processedRules) {
+            if (isSubDir(rule.getFrom(), processed.getFrom())) {
+              iterator.remove();
+              break;
+            }
+          }
+
+          processedRules.add(rule);
+        }
+
+        myRootPaths = new HashSet<File>();
+        for (FileRule rule : resultRules) {
+          //noinspection ConstantConditions
+          myRootPaths.add(new File((SystemInfo.isWindows ? "" : "/") + rule.getFrom()));
         }
       }
-
-      processedRules.add(rule);
-    }
-
-    myRootIncludePaths = new HashSet<File>();
-    for (FileRule rule : resultRules) {
-      myRootIncludePaths.add(new File((SystemInfo.isWindows ? "" : "/") + rule.getFrom()));
-    }
-    return myRootIncludePaths;
+    };
   }
 
   @NotNull
-  public Set<File> getPaths() {
-    if (myRootIncludePaths == null) initRootIncludePaths();
-    return myRootIncludePaths;
+  private static String resolvePath(@NotNull String path, @Nullable File baseDir) {
+    if (baseDir != null) {
+      return FileUtil.resolvePath(baseDir, path).getPath();
+    }
+    return path;
+  }
+
+  public Collection<File> getPaths() {
+    return myRootPaths;
+  }
+
+  public List<String> getBody() {
+    return myRules.getBody();
   }
 
   public boolean shouldInclude(@NotNull File path) {
-    return super.shouldInclude(SystemInfo.isWindows ? path.getPath() : path.getPath().substring(1));
+    return myRules.shouldInclude(SystemInfo.isWindows ? path.getPath() : path.getPath().substring(1));
   }
 }
